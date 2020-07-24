e08a14ae1017f6a7f43ff9bad0d5cc30c17067be
Leonid Shlyapnikov <leonid.shlyapnikov@digitalasset.com>
2020-07-24 18:33:38 -0400

    adding gatling dependencies

--- ledger-service/http-json-perf/src/main/scala/com/daml/http/perf/CommandsScenario.scala
+++ ledger-service/http-json-perf/src/main/scala/com/daml/http/perf/CommandsScenario.scala
@@ -1,6 +1,9 @@
 package com.daml.http.perf
 
+import com.daml.http.Statement
 import com.typesafe.scalalogging.StrictLogging
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
 
 import scala.concurrent.Future
 
@@ -11,4 +14,25 @@ object CommandsScenario extends StrictLogging {
     ()
   }
 
+  class CommandsSimulation extends Simulation {
+    val httpProtocol = http // 4
+      .baseUrl("http://computer-database.gatling.io") // 5
+      .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // 6
+      .doNotTrackHeader("1")
+      .acceptLanguageHeader("en-US,en;q=0.5")
+      .acceptEncodingHeader("gzip, deflate")
+      .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
+
+    val scn = scenario("BasicSimulation") // 7
+      .exec(
+        http("request_1") // 8
+          .get("/")) // 9
+      .pause(5) // 10
+
+    Statement.discard {
+      setUp( // 11
+        scn.inject(atOnceUsers(1)) // 12
+      ).protocols(httpProtocol) // 13
+    }
+  }
 }
